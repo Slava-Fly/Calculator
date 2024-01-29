1a7716aaa7d5bea0767b28f8efb6d19a3d76dae7
@Slava-Fly <school.94@bk.ru>
2024-01-29 14:28:31 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by User on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
